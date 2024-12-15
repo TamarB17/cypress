88b4d4186a5e0eb410e94b05c18c696157dc61df
TamarB17 <tamarbendeliani17@gmail.com>
2024-12-15 20:54:26 +0400

    testCY

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,38 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('Registration', (testData) => {
+  cy.get('#customer_menu_top > li > a').click();
+  cy.get('#accountFrm_accountregister').should('be.checked');
+  cy.get('#accountFrm > fieldset > .btn').click();
+  cy.get('#AccountFrm_firstname').type(testData);
+  cy.get('#AccountFrm_lastname').type(testData);
+  cy.get('#AccountFrm_email').type(testData + '@gmail.com');
+  cy.get('#AccountFrm_telephone').type(3454656);
+  cy.get('#AccountFrm_fax').type(345);
+  cy.get('#AccountFrm_company').type(testData);
+  cy.get('#AccountFrm_address_1').type(testData);
+  cy.get('#AccountFrm_city').type(testData);
+  cy.get('#AccountFrm_postcode').type(2341);
+  cy.get('#AccountFrm_country_id').select('Angola');
+  cy.get('#AccountFrm_zone_id').select('131');
+  cy.get('#AccountFrm_loginname').type(testData);
+  cy.get('#AccountFrm_password').type(testData);
+  cy.get('#AccountFrm_confirm').type(testData);
+  cy.get('#AccountFrm_newsletter1').click();
+  cy.get('#AccountFrm_agree').check();
+  cy.get('.btn').contains('Continue').click();
+});
+Cypress.Commands.add('login', (testData) => {
+  cy.get('#customer_menu_top > li > a').click();
+  cy.contains('Returning Customer').should('be.visible');
+  cy.get('#loginFrm_loginname').type(testData);
+  cy.get('#loginFrm_password').type(testData);
+  cy.get(
+    '[href="https://automationteststore.com/index.php?rt=account/forgotten/password"]'
+  ).should('have.text', 'Forgot your password?');
+  cy.get(
+    '[href="https://automationteststore.com/index.php?rt=account/forgotten/loginname"]'
+  ).should('have.text', 'Forgot your login?');
+  cy.get('#loginFrm > fieldset > .btn').click();
+});
